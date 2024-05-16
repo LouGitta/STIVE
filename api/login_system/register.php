0463dfa3f658506fb275cbec0f2ddcdff9e76355
ArthurB <Ruhtra.liopmob@gmail.com>
2024-05-16 11:06:26 +0200

    Update register.php
    test login ça marche pas

--- api/login_system/register.php
+++ api/login_system/register.php
@@ -1,1 +1,41 @@
+<?php
+if (isset($_POST['register'])) {
+    $mysqli = new mysqli("localhost", "root", "", "stive");
 
+    if ($mysqli->connect_error) {
+        die("Connection failed: " . $mysqli->connect_error);
+    }
+
+    $stmt = $mysqli->prepare("INSERT INTO users (nom, prenom, mail, mdp) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $nom, $prenom, $mail, $password);
+
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['mail'];
+    $password = $_POST['mdp'];
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    if ($stmt->execute()) {
+        echo "CA MARCHE!";
+    } else {
+        echo "Erreur : " . $stmt->error;
+    }
+
+    $stmt->close();
+    $mysqli->close();
+}
+?>
+
+
+
+<form action="register.php" method="post">
+  <label for="nom">Nom:</label> 
+  <input id="nom" name="nom" required="" type="text" />
+<label for="prenom">Prenom:</label> 
+  <input id="prenom" name="prenom" required="" type="text" />
+  <label for="mail">Mail:</label>
+  <input id="mail" name="mail" required="" type="email" />
+  <label for="mdp">Mdp:</label>
+  <input id="mdp" name="mdp" required="" type="password" />
+  <input name="register" type="submit" value="Register" />
+</form>
