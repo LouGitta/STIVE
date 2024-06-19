e4c8220598ebcd8d14685cedd3c2514c20e801b6
Lou <124198439+LouGitta@users.noreply.github.com>
2024-06-19 16:04:12 +0200

    Update RestockController.php

--- api/controllers/RestockController.php
+++ api/controllers/RestockController.php
@@ -4,12 +4,14 @@ header('Content-Type: application/json; charset=utf-8');
 require_once __DIR__ . '/../inc/config.inc.php';
 require_once __DIR__ . '/../inc/models/Model.php';
 
-
+// Classe qui regroupe les actions des Restock
 class RestockController {
-
+    // Attributs de la table restock
     public $att_restock = ['id','reference_commande', 'produit_id', 'date', 'quantite', 'prix'];
 
+    // Actions de la methode GET
     function get($param){
+        // Si paramètre id est défini alors récupère le restock correspondant
         if (isset($param['id'])) {
             
             $restock = Restock::find_one($param['id']);
@@ -19,7 +21,7 @@ class RestockController {
                 http_response_code(404);
                 echo json_encode(["error" => "Restock non trouve"]);
             }
-           
+        // Sinon récupère tous les restock
         } else {
 
             $restocks = Restock::find_many();
@@ -35,6 +37,7 @@ class RestockController {
         }
     }
 
+    // Action de restock qui est appelé depuis action post de commande
     function restock($data){
         $reference = date('Y-m-d') .'/'. substr(hash('sha256', date('Y-m-d H:i:s:ms')), 8, 8);
 
@@ -57,12 +60,11 @@ class RestockController {
             $restock->save();
             $this->stockUpdate($data);
     }
-
+    // Mise à jour du stock après commande de restock
     function stockUpdate($data){
         $produit = Produit::find_one($data->produit_id);
-        // print_r($produit->quantite_stock);
+
         $produit->quantite_stock += 150;
-        // print_r($produit->quantite_stock);
 
         $produit->save();
         // echo json_encode(['status' => 'success', 'message' => "Restock valide"]);
